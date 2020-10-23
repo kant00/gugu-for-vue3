aa24596d7097e5bcbbf9d2b2801b68bde846a435
kant0 <1195510518@qq.com>
2020-10-23 16:13:26 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+<div>hi<div>
+        <liu />
+</template>
+
+<script>
+import liu from "./components/liu.vue";
+
+export default {
+    name: "App",
+    components: {
+        liu,
+    },
+};
+</script>
